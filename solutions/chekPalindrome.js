be6219edc4ca8a4b6a75939dd56b72827933cd04
greys <s838greyss@gmail.com>
2019-04-05 10:59:55 -0400

    es

---
+++ solutions/chekPalindrome.js
@@ -0,0 +1,13 @@
+function checkPalindrome(str) {
+    low = str.toLowerCase();
+    regExp = low.replace(/[{\W_]/g, "");
+    s = regExp.split("");
+    r = s.reverse();
+    j = r.join("");
+    if (regExp == j)
+       {return true;}
+    else {return false;}
+}
+console.log(checkPalindrome("aabaa"));
+console.log(checkPalindrome("abac"));
+console.log(checkPalindrome("a"));
