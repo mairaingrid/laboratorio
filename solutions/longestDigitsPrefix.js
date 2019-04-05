57916da93313bcf348601d28ef609a349ba17c73
Maira Ramirez <ingrid24may@hotmail.com>
2019-04-04 23:19:51 -0400

    ejercicio5

---
+++ solutions/longestDigitsPrefix.js
@@ -0,0 +1,11 @@
+function longestDigitsPrefix(inputString) {
+    var temp = "";
+    if (inputString.length == 0) return "";
+    for(var i = 0;i < inputString.length;i++){
+        if(inputString[i] > '9' || inputString[i] < '0') break;
+        else temp += inputString[i];
+    }
+    return temp;
+}
+
+console.log(longestDigitsPrefix("123aa1"));
